4965f3143f6cbb80277bd142a6965ba441d0b3a2
George Ribarski <george.ribarski@progress.com>
2018-07-30 17:38:24 +0300

    Dev Commit

--- app.js
+++ app.js
@@ -37,5 +37,5 @@ var bot = new builder.UniversalBot(connector);
 bot.set('storage', tableStorage);
 
 bot.dialog('/', function (session) {
-    session.send('B0l7 repeats: ' + session.message.text);
+    session.send('B0l7 dev_repeats: ' + session.message.text);
 });
