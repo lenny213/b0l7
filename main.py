e0829e59b9c69b42c88e6c11aa392e461c2ff5b1
George Ribarski <george.ribarski@progress.com>
2018-08-03 16:48:13 +0300

    echo-bot-with-state

--- main.py
+++ main.py
@@ -1,84 +1,95 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-import http.server
-import json
-import asyncio
-from botbuilder.schema import (Activity, ActivityTypes, ChannelAccount)
-from botframework.connector import ConnectorClient
-from botframework.connector.auth import (MicrosoftAppCredentials,JwtTokenValidation, SimpleCredentialProvider)
+"""
+This sample shows how to create a simple EchoBot with state.
+"""
 
-# import secret vars from separate file
+
+from aiohttp import web
+from botbuilder.schema import (Activity, ActivityTypes)
+from botbuilder.core import (BotFrameworkAdapter, BotFrameworkAdapterSettings, TurnContext,
+                             ConversationState, MemoryStorage, UserState)
+
 import secret
 
-class BotRequestHandler(http.server.BaseHTTPRequestHandler):
+APP_ID = 'secret.APP_ID'
+APP_PASSWORD = 'secret.APP_PASSWORD'
+PORT = 10100
+SETTINGS = BotFrameworkAdapterSettings(APP_ID, APP_PASSWORD)
+ADAPTER = BotFrameworkAdapter(SETTINGS)
 
-    @staticmethod
-    def __create_reply_activity(request_activity, text):
-        return Activity(
-            type=ActivityTypes.message,
-            channel_id=request_activity.channel_id,
-            conversation=request_activity.conversation,
-            recipient=request_activity.from_property,
-            from_property=request_activity.recipient,
-            text=text,
-            service_url=request_activity.service_url)
+# Create MemoryStorage, UserState and ConversationState
+memory = MemoryStorage()
+# Commented out user_state because it's not being used.
+# user_state = UserState(memory)
+conversation_state = ConversationState(memory)
 
-    def __handle_conversation_update_activity(self, activity):
-        self.send_response(202)
-        self.end_headers()
-        if activity.members_added[0].id != activity.recipient.id:
-            credentials = MicrosoftAppCredentials(secret.APP_ID, secret.APP_PASSWORD)
-            reply = BotRequestHandler.__create_reply_activity(activity, 'Hello and welcome to the echo bot!')
-            connector = ConnectorClient(credentials, base_url=reply.service_url)
-            connector.conversations.send_to_conversation(reply.conversation.id, reply)
+# Register both State middleware on the adapter.
+# Commented out user_state because it's not being used.
+# ADAPTER.use(user_state)
+ADAPTER.use(conversation_state)
 
-    def __handle_message_activity(self, activity):
-        self.send_response(200)
-        self.end_headers()
-        credentials = MicrosoftAppCredentials(secret.APP_ID, secret.APP_PASSWORD)
-        connector = ConnectorClient(credentials, base_url=activity.service_url)
-        reply = BotRequestHandler.__create_reply_activity(activity, 'You said: %s' % activity.text)
-        connector.conversations.send_to_conversation(reply.conversation.id, reply)
 
-    def __handle_authentication(self, activity):
-        credential_provider = SimpleCredentialProvider(secret.APP_ID, secret.APP_PASSWORD)
-        loop = asyncio.new_event_loop()
-        try:
-            loop.run_until_complete(JwtTokenValidation.authenticate_request(
-                activity, self.headers.get("Authorization"), credential_provider))
-            return True
-        except Exception as ex:
-            self.send_response(401, ex)
-            self.end_headers()
-            return False
-        finally:
-            loop.close()
+async def create_reply_activity(request_activity, text) -> Activity:
+    return Activity(
+        type=ActivityTypes.message,
+        channel_id=request_activity.channel_id,
+        conversation=request_activity.conversation,
+        recipient=request_activity.from_property,
+        from_property=request_activity.recipient,
+        text=text,
+        service_url=request_activity.service_url)
 
-    def __unhandled_activity(self):
-        self.send_response(404)
-        self.end_headers()
 
-    def do_POST(self):
-        body = self.rfile.read(int(self.headers['Content-Length']))
-        data = json.loads(str(body, 'utf-8'))
-        activity = Activity.deserialize(data)
+async def handle_message(context: TurnContext) -> web.Response:
+    # Access the state for the conversation between the user and the bot.
+    state = await conversation_state.get(context)
 
-        if not self.__handle_authentication(activity):
-            return
+    if hasattr(state, 'counter'):
+        state.counter += 1
+    else:
+        state.counter = 1
 
-        if activity.type == ActivityTypes.conversation_update.value:
-            self.__handle_conversation_update_activity(activity)
-        elif activity.type == ActivityTypes.message.value:
-            self.__handle_message_activity(activity)
-        else:
-            self.__unhandled_activity()
+    response = await create_reply_activity(context.activity, f'{state.counter}: You said {context.activity.text}.')
+    await context.send_activity(response)
+    return web.Response(status=202)
 
+
+async def handle_conversation_update(context: TurnContext) -> web.Response:
+    if context.activity.members_added[0].id != context.activity.recipient.id:
+        response = await create_reply_activity(context.activity, 'Welcome to the Echo Adapter Bot!')
+        await context.send_activity(response)
+    return web.Response(status=200)
+
+
+async def unhandled_activity() -> web.Response:
+    return web.Response(status=404)
+
+
+async def request_handler(context: TurnContext) -> web.Response:
+    if context.activity.type == 'message':
+        return await handle_message(context)
+    elif context.activity.type == 'conversationUpdate':
+        return await handle_conversation_update(context)
+    else:
+        return await unhandled_activity()
+
+
+async def messages(req: web.web_request) -> web.Response:
+    body = await req.json()
+    activity = Activity().deserialize(body)
+    auth_header = req.headers['Authorization'] if 'Authorization' in req.headers else ''
+    try:
+        return await ADAPTER.process_activity(activity, auth_header, request_handler)
+    except Exception as e:
+        raise e
+
+
+app = web.Application()
+app.router.add_post('/', messages)
 
 try:
-    SERVER = http.server.HTTPServer(('0.0.0.0', 10100), BotRequestHandler)
-    print('Started http server')
-    SERVER.serve_forever()
-except KeyboardInterrupt:
-    print('^C received, shutting down server')
-    SERVER.socket.close()
+    web.run_app(app, host='0.0.0.0', port=PORT)
+except Exception as e:
+    raise e
